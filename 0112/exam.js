09f377df6e8429911d0090280856c54793bfcebf
gh9727 <gj9727@gmail.com>
2024-01-12 19:20:55 +0900

    study:0112

--- 0112/exam.js
+++ 0112/exam.js
@@ -3,7 +3,7 @@ $('tr td:last-child').css('color', 'blue');
 
 $('td').on('click', function () {
     // td 요소 정보를 변수에 담기
-    let td_this = $(this); // 배열처럼 쌓이는가?
+    let td_this = $(this); // 스택처럼 쌓이는게 맞다
     let value = $(this).text();
     $('#day').val(`2022.7.${value}`);
 
@@ -13,5 +13,7 @@ $('td').on('click', function () {
         // 위의 td_this가 초기화가 되어도 이전 td 요소 정보도 같이 딸려와서
         // td 요소에 input 데이터를 넣어준 뒤 td_this 변수 null로 초기화
         td_this = null;
+        $('#day').val(' ');
+        $('#content').val(' ');
     });
 });
